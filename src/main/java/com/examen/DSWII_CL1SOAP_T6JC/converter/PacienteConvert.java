fc98b6b8b33a2e5d652f2b844917f0ae32ae7623
Bruno-xd <gonzalesramirezbruno@gmail.com>
2023-09-29 22:01:21 -0500

    Cosas

--- src/main/java/com/examen/DSWII_CL1SOAP_T6JC/converter/PacienteConvert.java
+++ src/main/java/com/examen/DSWII_CL1SOAP_T6JC/converter/PacienteConvert.java
@@ -1,4 +1,29 @@
 package com.examen.DSWII_CL1SOAP_T6JC.converter;
 
+import com.examen.DSWII_CL1SOAP_T6JC.model.Paciente;
+import com.examen.dswll_cl1soap_t6jc.ws.objects.Pacientews;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PacienteConvert {
+//    public Paciente convertPacienteWsToPaciente(Pacientews pacientews){
+//        Paciente paciente = new Paciente();
+//        paciente.setIdpaciente(pacientews.getIdpaciente());
+//        paciente.setNompaciente(pacientews.getNompaciente());
+//        paciente.setApepaciente(pacientews.getApepaciente());
+//        paciente.setDocpaciente(pacientews.getDocpaciente());
+//        paciente.setFechanacpaciente(pacientews.getFechanacpaciente());
+//        paciente.setEmailpaciente(pacientews.getEmailpaciente());
+//        return paciente;
+//    }
+//    public Pacientews convertPacienteToPacienteWs(Paciente paciente){
+//        Pacientews pacientews = new Pacientews();
+//        pacientews.setIdpaciente(paciente.getIdpaciente());
+//        pacientews.setNompaciente(paciente.getNompaciente());
+//        pacientews.setApepaciente(paciente.getApepaciente());
+//        pacientews.setDocpaciente(paciente.getDocpaciente());
+//        pacientews.setFechanacpaciente(paciente.getFechanacpaciente());
+//        pacientews.setEmailpaciente(paciente.getEmailpaciente());
+//        return pacientews;
+//    }
 }
