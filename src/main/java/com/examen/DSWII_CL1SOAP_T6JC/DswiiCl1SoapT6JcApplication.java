4cd3b1dcdbcadfab9b38bad8f1a3327004a17069
Bruno-xd <gonzalesramirezbruno@gmail.com>
2023-09-29 20:44:10 -0500

    First commit

---
+++ src/main/java/com/examen/DSWII_CL1SOAP_T6JC/DswiiCl1SoapT6JcApplication.java
@@ -0,0 +1,13 @@
+package com.examen.DSWII_CL1SOAP_T6JC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapT6JcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapT6JcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/DSWII_CL1SOAP_T6JC/DswiiCl1SoapT6JcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.DSWII_CL1SOAP_T6JC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapT6JcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
